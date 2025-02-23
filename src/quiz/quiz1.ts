e902f306226a712868eb54d5f9bbb8944aa77f39
geogeos <ggeogeos@gmail.com>
2025-02-23 23:59:03 +0100

    feat : Ajoute un premier quiz RXJS

---
+++ src/quiz/quiz1.ts
@@ -0,0 +1,81 @@
+import { concatAll, delay, exhaustAll, from, interval, map, merge, mergeAll, mergeMap, Observable, of, switchAll, take, tap, throwError } from "rxjs";
+
+
+/**
+* La question est toujorus la même : qu'est-ce qu'il y aura d'affiché dans la console (et dans quel ordre) ?
+*/
+export default class Quiz1 {
+    question1(): void {
+        of("toto").pipe(
+            map((t) => t.toUpperCase())
+        ).subscribe({
+            next: (r) => console.log(r),
+            error: console.log,
+            complete: () => console.log('COMPLETE')
+        })
+    }
+
+    question2(): void {
+        merge(of("toto"), of("tata")).pipe(
+            map((t) => t.toUpperCase())
+        ).subscribe({
+            next: (r) => console.log(r),
+            error: console.log,
+            complete: () => console.log('COMPLETE')
+        })
+    }
+
+    question3(): void {
+        interval(10).pipe(
+            take(5)
+        ).subscribe({
+            next: (r) => console.log(r),
+            error: console.log,
+            complete: () => console.log('COMPLETE')
+        })
+    }
+
+    private mettreAuCarreAvecDelai$(i: number): Observable<number> {
+        return of(undefined).pipe(
+            delay(i <= 4 ? 100 : 0),
+            map(() => i*i)
+        );
+    }
+
+    question4(): void {
+        of(1, 2, 3, 4, 5, 6, 7, 8, 9).pipe(
+            map((i) => this.mettreAuCarreAvecDelai$(i)),
+        ).subscribe({
+            next: (r) => console.log(r),
+            error: console.log,
+            complete: () => console.log('COMPLETE')
+        })
+    }
+
+    question5(): void {
+        from(Promise.resolve()).subscribe({
+            next: (r) => console.log(r),
+            error: console.log,
+            complete: () => console.log('COMPLETE')
+        })
+    }
+
+    question6(): void {
+        of(10).pipe(
+            delay(10),
+            tap(() => console.log('Coucou')),
+            map(() => {
+                throw new Error('toto');
+            }),
+            tap(() => console.log('Au revoir')),
+        ).subscribe({
+            next: (r) => console.log(r),
+            error: console.log,
+            complete: () => console.log('COMPLETE')
+        })
+    }
+
+    
+}
+
+const quiz = new Quiz1();
